0992d4d06d45d3842c3eaca0152c1a25aee5d5ae
Amminn <55767303+Amminn@users.noreply.github.com>
2021-04-21 13:51:07 +0100

    Update index.js

--- Snake game/index.js
+++ Snake game/index.js
@@ -2,11 +2,14 @@ const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 let squares = []
-let currentSnake = [5,4,3]
+let currentSnake = [2,1,0]
+let direction = 1
+const width = 10
+let appleIndex = 0
 
 function createGrid() {
     //create 100 of these elements with a for loop
-    for (let i=0; i < 100; i++) {
+    for (let i=0; i < width*width; i++) {
      //create element
     const square = document.createElement('div')
     //add styling to the element
@@ -17,22 +20,82 @@ function createGrid() {
     squares.push(square)
     }
 }
-
 createGrid()
 
-console.log(squares)
+currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
+    if (
+        (currentSnake[0] + width >= width*width && direction === width) || //if snake has hit bottom
+        (currentSnake[0] % width === width-1 && direction === 1) || //if snake has hit right wall
+        (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
+        (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+    return clearInterval(timerId)
+
     //remove last element from our currentSnake array
     const tail = currentSnake.pop()
-    console.log(tail)
-    console.log(currentSnake)
     //remove styling from last element
-    console.log(squares[tail])
     squares[tail].classList.remove('snake')
     //add square in direction we are heading
-    currentSnake.unshift()
+    currentSnake.unshift(currentSnake[0] + direction)
     //add styling so we can see it
     
+    //deal with snake head gets apple
+    if () {
+        //remove the class of apple
+        
+        //grow our snake by adding class of snake to it
+        
+        //grow our snake array
+        
+        //generate new apple
+        
+        //add one to the score
+        
+        //display our score
+        
+        //speed up our snake
+        
+    }
+    
+    
+    
+    squares[currentSnake[0]].classList.add('snake')
 }
 move()
+
+let timerId = setInterval(move, 1000)
+
+
+
+function generateApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+} 
+generateApple()
+
+// 39 is right arrow
+// 38 is for the up arrow
+// 37 is for the left arrow
+// 40 is for the down arrow
+
+function control(e) {
+    if (e.keyCode === 39) {
+        console.log('right pressed')
+        direction = 1
+    } else if (e.keyCode === 38) {
+        console.log('up pressed')
+        direction = -width
+    } else if (e.keyCode === 37) {
+        console.log('left pressed')
+        direction = -1
+    } else if (e.keyCode === 40) {
+        console.log('down pressed')
+        direction = +width
+    }
+}
+document.addEventListener('keyup', control)
